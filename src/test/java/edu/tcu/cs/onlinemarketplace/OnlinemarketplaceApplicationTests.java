5cc3fd574ad92ee8bc23515cdb288f311335665a
fjosea10 <fjosea10@gmail.com>
2023-09-11 18:15:43 -0500

    initial commit

---
+++ src/test/java/edu/tcu/cs/onlinemarketplace/OnlinemarketplaceApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.tcu.cs.onlinemarketplace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinemarketplaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
